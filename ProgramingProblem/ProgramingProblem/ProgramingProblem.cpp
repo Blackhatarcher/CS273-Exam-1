08be370f6bf90bf99a100acf34c45e7f06b290f0
christianwilliams20 <christianwilliams20@LAB-SCI-304-10>
2017-03-03 13:20:37 -0800

    copy pasted main.cpp code

--- ProgramingProblem/ProgramingProblem/ProgramingProblem.cpp
+++ ProgramingProblem/ProgramingProblem/ProgramingProblem.cpp
@@ -2,10 +2,86 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
+using std::cout;
+using std::endl;
+using std::string;
 
+namespace Simulation // NAMESPACE
+{
+	// STEP 1: Complete the implementation of the Aircraft base class 
+	// as described in the UML diagram
+	class Aircraft
+	{
+	protected:
+		int num_engines;
+	public:
+		Aircraft(int num_engines) {
+			// STEP 1: Complete the constructor in itialize member variable
+		};
+		virtual void Description() = 0;      // Description is a pure virtual function
+	};
+
+	// STEP 2: Complete the Engine class
+	class Engine
+	{
+	private:
+		string manuf;
+	public:
+		Engine() {}
+		void set_manuf(string manuf) {
+			// STEP 2: initialize the member variable
+		}
+	};
+
+	// STEP 3:  Define the Glider class as described
+	// in the UML diagram.   For this test, DO NOT separate
+	// the class implementations into separate cpp and header files.
+	class Glider : public Aircraft {
+	public:
+		// STEP 3: Complete the Glider Constructor
+
+		// STEP 3: Complete the Glider Description() method
+	};
+
+	// STEP 4:  Define the Boeing777 class as described
+	// in the UML diagram.  
+	class Boeing777 : public Aircraft {
+	private:
+		// STEP 4: Define the member variables for the 777
+	public:
+
+		// STEP 4: Complete the Boeing777 Constructor
+
+		// STEP 4: Complete the Boeing777 Description Method
+	};
+}
 
 int main()
 {
-    return 0;
-}
+	Simulation::Aircraft * Airport[4];
 
+	// STEP 5: Assign ONE new Glider AND ONE new Boeing777 object to each of the two elements
+	// in the Aiport array.  Remember the classes are defined in the namespace Simulation.
+	Airport[0] = new Simulation::Glider();
+	Airport[1] = new Simulation::Boeing777(2, "Blue");
+
+	// STEP 6: Create a new class in the Simulation for GenericAircraft.
+	// It should derive from Aircraft. It should have the number of
+	// engines and aircraft name be parameters passed to the
+	// constructor.
+
+	// STEP 7: Create two new instances of your GenericAircraft class
+	// and add them to the Airport array.
+	// The first instance should be an F18 with 4 engines
+	// and the second one should be an F16 with 2 engines.
+
+	for (int i = 0; i < 4; ++i)
+	{
+		// This line will output a description of each aircraft
+		Airport[i]->Description();
+	}
+
+	return 0;
+}
